4e51c59f6b899e8a8d9f8b8b94167d4cda44bef8
fuzzybabybunny <victor@victorlinphoto.com>
2014-09-01 11:51:29 +0800

    promise actually not returning value, got sidebar working

--- client/views/pages/test/test.js
+++ client/views/pages/test/test.js
@@ -20,51 +20,48 @@ Template.test.rendered = function(){
 // });
 
 
-var geocode = new Promise(function(resolve, reject) {
+geocodeAddress = function(streetAddress){
 
-	// async call to Google's Geocoder - takes street address
-	// and returns GPS coordinates
-  var address = "1600 Amphitheatre Pkwy, Mountain View, CA 94043";
-  var gpsPosition = {};
-  geocoder = new google.maps.Geocoder();
-  if (geocoder) {
-    geocoder.geocode({'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[0]) {
-          console.log("got results!");
-          gpsPosition.B = results[0].geometry.location['B'];
-          gpsPosition.k = results[0].geometry.location['k'];
-          console.log(gpsPosition);
-          resolve(gpsPosition);
-        } else {
-          alert("No results found");
-          reject(Error("It broke"));
-        }
-      } else {
-        alert("Geocoder failed due to: " + status);
-        reject(Error("It broke"));
-      }
-    });
-  }
+	var geocodeObject = new Promise(function(resolve, reject) {
 
- //  // this part just isn't working
- //  // what should go inside this if statement to check
- //  // if the async call worked?
-	// if (!!gpsPosition.B/* everything turned out fine */) {
- //    resolve("Stuff worked!");
- //  }
- //  else {
- //    reject(Error("It broke"));
- //  }
+		// async call to Google's Geocoder - takes street address
+		// and returns GPS coordinates
+	  var gpsPosition = {};
+	  geocoder = new google.maps.Geocoder();
+	  if (geocoder) {
+	  	// console.log("This is the address");
+	  	// console.log(streetAddress);
+	    geocoder.geocode({'address': streetAddress}, function(results, status) {
+	      if (status == google.maps.GeocoderStatus.OK) {
+	        if (results[0]) {
+	          console.log("got results!");
+	          gpsPosition.B = results[0].geometry.location['B'];
+	          gpsPosition.k = results[0].geometry.location['k'];
+	          console.log(gpsPosition);
+	          resolve(gpsPosition);
+	        } else {
+	          alert("No results found");
+	          reject(Error("It broke"));
+	        }
+	      } else {
+	        alert("Geocoder failed due to: " + status);
+	        reject(Error("It broke"));
+	      }
+	    });
+	  }
 
-});
+	});
 
-geocode.then(function(result) {
-	console.log("This is the gpsPosition");
-  console.log(result); // "Stuff worked!"
-}, function(err) {
-  console.log(err); // Error: "It broke"
-});
+	geocodeObject.then(function(result) {
+		console.log("This is the gpsPosition");
+		console.log(result); // this console logs the result just fine
+	  return result; // however the result can't be returned
+	}, function(err) {
+	  console.log(err); // Error: "It broke"
+	});
+
+}
+
 
 
 
